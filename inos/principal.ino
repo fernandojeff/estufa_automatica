0a9e68a9bb6b39ca46d47b84adf9771094f412c9
Fernando Jefferson <71847546+fernandojeff@users.noreply.github.com>
2023-06-28 22:26:21 -0300

    Comentando linhas

--- inos/principal.ino
+++ inos/principal.ino
@@ -1,30 +1,37 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <ESPmDNS.h>
-#include <DHT.h>
+#include "DHT.h"
 #include "ESP32_Servo.h"
 
 // Set consts
 // WiFi Connection set
 
-#define SSID "SEU SSID"
-#define PASSWORD "SUA SENHA"
+#define SSID "SEU SSDI" //nome da rede
+#define PASSWORD "SUA SENHA" // senha da sua rede
 
 // MQTT Connection set 
 
-#define MQTT_ID "CRIE UM ID"
-#define MQTT_BROKER "broker.hivemq.com"
+#define MQTT_ID "SEU ID" //Define o ID do ESP 
+#define MQTT_BROKER "broker.hivemq.com" // broker grátis
 #define MQTT_PORT 1883
-#define MQTT_NOME_TOPIC "SEU TOPICO"
+#define MQTT_MILLIS_TOPIC "ifrj_cnit_estufatcc_millis" //topico millis 
+#define MQTT_MOTOR_TOPIC "ifrj_cnit_motor"
+#define MQTT_BOMBA_TOPIC "ifrj_cnit_bomba"
+#define MQTT_UMISOLO_TOPIC "ifrj_cnit_umisolo"
+#define MQTT_LIGHT_TOPIC "ifrj_cnit_light"
+#define MQTT_TEMP_TOPIC "ifrj_cnit_temperatura" //topico da temperatura
+#define MQTT_UMI_TOPIC "ifrj_cnit_umiar" //topico da umidade do ar
+
 
 // Set ports
 #define DHTPIN 12 //sensor DHT
 #define PIN_MOTOR1 27 //define porta do servo
-#define pinUS 34
-#define pinLDR 35 
-const int RelePin = 23; 
+#define pinUS 34 // pino sensor de umidade do solo
+#define pinLDR 35 // pino sensor LDR (luminosidade)
+const int RelePin = 23; // pino do rele
 
-// Servo motor1;
+Servo motor1; // cria o servo motor
 
 // Set the type of DHT sensor
 #define DHTTYPE DHT11 
@@ -73,7 +80,7 @@ void setupMQTT(){
     // Config MQTT Broker connection
 
     MQTT.setServer(MQTT_BROKER, MQTT_PORT);
-    MQTT.setCallback(mqtt_ifrj_callback); 
+    MQTT.setCallback(mqtt_callback); // seta callback
 
     // Conn exec
 
@@ -83,7 +90,7 @@ void setupMQTT(){
 
         if(MQTT.connect(MQTT_ID)){
             Serial.println("- MQTT Setup: Conectado com sucesso");
-             MQTT.subscribe(MQTT_MOTOR_TOPIC);
+             MQTT.subscribe(MQTT_MOTOR_TOPIC); // lê o topico
              MQTT.subscribe(MQTT_BOMBA_TOPIC);
         } else {
             Serial.println("- MQTT Setup: Falha ao se conectar, tentando novamente em 2s");
@@ -102,7 +109,7 @@ void setup(){
     pinMode(pinLDR, INPUT);
 
 
-    // motor1.attach(PIN_MOTOR1, 500, 2400);
+    motor1.attach(PIN_MOTOR1, 500, 2400);
     digitalWrite(RelePin, HIGH); 
 
     // Call setup wifi
@@ -127,8 +134,8 @@ void loop(){
     int porcentoL = map(luminosidade, 0, 4095, 0, 100);
     int porcentoUs = map(umidadeS, 4095, 0, 0, 100);
 
-    MQTT.publish(MQTT_UMISOLO_TOPIC, String(porcentoUs).c_str());
-    MQTT.publish(MQTT_LIGHT_TOPIC, String(porcentoL).c_str());
+    MQTT.publish(MQTT_UMISOLO_TOPIC, String(porcentoUs).c_str()); // publica umidade do solo
+    MQTT.publish(MQTT_LIGHT_TOPIC, String(porcentoL).c_str()); // publica luminosidade
 
     //Publica a TEMPERATURA
     if (isnan(t)) { //verifica se o sensor está mandando numero
@@ -143,6 +150,16 @@ void loop(){
       return;
     } else {
       MQTT.publish(MQTT_UMI_TOPIC, String(h, 0).c_str());
+    }
+
+    if (porcentoUs <= 10) {
+      digitalWrite(RelePin, LOW);  //liga o pino
+      Serial.println("LIGANDO BOMBA");
+      delay(1000);
+      digitalWrite(RelePin, HIGH); //desliga
+    } else {
+      digitalWrite(RelePin, HIGH); //aciona o pino
+      Serial.println("DESLIGANDO BOMBA");
     }
 
     setupWIFI();
@@ -166,19 +183,19 @@ void mqtt_ifrj_callback(char* topic, byte* payload, unsigned int length)
 
   Serial.println(msg);
   //Controlando teto motor 
-  // if (msg.equals("M1")) {
-  //    for (int pos = 180; pos >= 0; pos -= 1) {
-  //     motor1.write(pos); //motor roda 160 graus
-  //     delay(5);
-  //     }
-  //     Serial.println("MOTOR INDO");
-  //   } else if (msg.equals("M0")) {
-  //     for (int pos = 0; pos <= 180; pos += 1) {
-  //     motor1.write(pos);
-  //     delay(5);
-  //     } 
-  //     Serial.println("MOTOR VOLTANDO");
-  //   }
+  if (msg.equals("M1")) {
+     for (int pos = 180; pos >= 0; pos -= 1) {
+      motor1.write(pos); //motor roda 160 graus
+      delay(5);
+      }
+      Serial.println("MOTOR INDO");
+    } else if (msg.equals("M0")) {
+      for (int pos = 0; pos <= 180; pos += 1) {
+      motor1.write(pos);
+      delay(5);
+      } 
+      Serial.println("MOTOR VOLTANDO");
+    }
   if (msg.equals("B1")) {
       digitalWrite(RelePin, LOW);  //desativa o pino
       Serial.println("LIGANDO BOMBA");
